cb29c39d4281604dc10d49e0159852b54aa6e19b
carlasdc <carlamarielsandoval@gmail.com>
2022-11-03 22:04:46 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Empleado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Empleado
+    {
+        public Empleado(int id, string nombre, string apellido)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            Comision = 10000;
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public static decimal comision = 1000;
+        public static decimal Comision
+        {
+            get
+            {
+                return comision;
+            }
+            set
+            {
+                comision = value;
+            }
+        }
+        
+
+    }
+}
---
+++ WindowsPrueba/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace WindowsPrueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrarCom_Click(object sender, EventArgs e)
+        {
+            Empleado empleado1 = new Empleado(0, "juan", "Perez"); 
+            Empleado empleado2= new Empleado(1, "Marin", "Perez");
+            MessageBox.Show(Empleado.Comision.ToString());
+            Empleado empleado3 = new Empleado(0, "julia", "Perez");
+            Empleado empleado4 = new Empleado(1, "julio", "Perez");
+            Empleado.Comision = 50000;
+            MessageBox.Show(Empleado.Comision.ToString());
+
+        }
+    }
+}
